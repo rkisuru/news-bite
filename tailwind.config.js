48e3262fa861102bb7296a47ed4578f710bde685
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-10-12 23:04:37 +0530

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
